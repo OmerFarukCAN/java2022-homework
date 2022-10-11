cb7f98c7e062391c225853465112a79bb6bc6731
OmerFarukCAN <omerfarukcaan@gmail.com>
2022-10-11 18:30:30 +0300

    javahomework-2

---
+++ inheritanceDemo/src/BaseKrediManager.java
@@ -0,0 +1,7 @@
+public class BaseKrediManager {
+    public void hesapla() {
+        System.out.println("Kredi Hesaplandi");
+    }
+}
+
+// A class can inherit only one other class
---
+++ inheritanceDemo/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        KrediUI krediUI = new KrediUI();
+        krediUI.krediHesapla(new OgretmenKrediManager());
+        krediUI.krediHesapla(new TarimKrediManager());
+        krediUI.krediHesapla(new AskerKrediManager());
+    }
+}
+
